def3b81cf89f16892593c71f2a33d3d6c4f5d433
David Cunningham <david.cunningham@immuta.com>
2024-04-25 13:06:47 -0400

    feat: test (#8)

--- apps/service/src/app/routes/root.ts
+++ apps/service/src/app/routes/root.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify';
 export default async function (fastify: FastifyInstance) {
   fastify.get('/', async function () {
     console.log('test');
+    console.log('test');
     return { message: 'Hello API' };
   });
 }
